0ff18b572df5d91356943a80176a8ef071102e03
Jacob Jonsson <jassob@live.se>
2015-04-03 00:41:46 +0200

    Added django documentation

--- dnollkse/urls.py
+++ dnollkse/urls.py
@@ -17,5 +17,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls'))
     url(r'^news/', include('news.urls')),
 )
