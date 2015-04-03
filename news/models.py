8cfe4282e34efcfa31c6e139131f567285e9d4fb
Jacob Jonsson <jassob@live.se>
2015-04-03 17:21:40 +0200

    Added convenience function for the models.
    Added functions to get all published posts, posts published during a
    month and posts published during a day.

--- news/models.py
+++ news/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timedelta
+from pytz import timezone
 # Django models
 from django.db import models
+
 
 """
 news.models module.
@@ -73,6 +76,94 @@ class Post(models.Model):
     author = models.ForeignKey(Author)
      # Whether the post is published or not
     published = models.BooleanField(default=False)
+
+    def publishedPosts():
+        """
+        Retrieves all published posts
+
+        All it does is filters out the unpublished posts.
+        """
+        return Post.objects.filter(published=True)
+
+    def byMonth(month):
+        """
+        Retrieve all posts published during a month.
+
+        To get the last day in the month a little hack is used, 
+        we get the date of the first day in next month (right before midnight)
+        and then we subtract one second from the date. 
+        This gives us the date and time for the last second in the month so
+        that we retrieve every post even from the last day.
+
+        Raises a ValueError if month is out of bounds.
+        """
+        # Create our timezone
+        sthlm = timezone("Europe/Stockholm")
+        year = datetime.now().year            
+        first_day = sthlm.localize(datetime(year, month, 1, 00, 00, 00))
+        
+        if month == 12:
+            # If we are in december we know the last day
+            last_day= sthlm.localize(datetime(year, 12, 31, 23, 59, 59))
+        else:
+            # Otherwise, last day = first day in next month - 1 second
+            tmp_day = sthlm.localize(datetime(year, month+1, 1, 00, 00, 00))
+            last_day = tmp_day - timedelta(seconds=1)
+            return Post.publishedPosts().filter(
+                pub_date__range=(first_day, last_day))
+        
+    def byDay(month, day):
+        """
+        Retrieves the posts published during a day in a month.
+        
+        Not really sure what use we could have of this functions, but convinience functions are always nice.
+        
+        Raises a ValueError if month or day are out of bounds.
+        """
+        posts_in_month = Post.byMonth(month)
+        sthlm = timezone("Europe/Stockholm")
+        year = datetime.now().year            
+        
+        start_time = sthlm.localize(datetime(year, month, day, 00, 00, 00))
+        end_time = sthlm.localize(datetime(year, month, day, 23, 59, 59))
+        return posts_in_month.filter(pub_date__range=(start_time, end_time))
+        
+    def publish(self):
+        """
+        Publishes a post.
+        
+        This function will publish a post and then save it, 
+        if the post already is published nothing happens.
+
+        >>> p = Post(title="T", content="Some text", author=0)
+        # Creates a new post
+        >>> p.publish()
+        # Sets its status to published and saves the post
+        """
+        if not self.published:
+            self.published = True
+            self.save()
+        # If self is published, nothing will happen
+        
+
+    def unpublish(self):
+        """ 
+        Unpublishes a post.
+
+        This function will unpublish a post and then save it,
+        if the post is not published when this function is called nothing will
+        happen.
+
+        >>> p = Post.objects.latest('pub_date')
+        # p = latest post (considering published date)
+        >>> p.unpublish()
+        # Sets its status to unpublished and saves the post
+        """
+
+        if self.published:
+            self.published = False
+            self.save()
+        # If self is not published, nothing will happen
 
     def __str__(self):
         """
