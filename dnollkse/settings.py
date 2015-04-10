09f3a7338fb765a859d7411cbea8e5ebc6e64068
Jacob Jonsson <jassob@live.se>
2015-04-10 17:21:18 +0200

    Configured project to use /static/ + more
    - Added a static files entry so django can find files in
    project_root/static/ directory.
    
    - Added a default database connection to a sqlite3 database file

--- dnollkse/settings.py
+++ dnollkse/settings.py
@@ -60,11 +60,8 @@ WSGI_APPLICATION = 'dnollkse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # We might change this to postgresql later
-        'NAME': '', # DB name
-        'HOST': '', # Host domain name
-        'USER': '', # DB username
-        'PASSWORD': '' # DB user password
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -86,5 +83,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
