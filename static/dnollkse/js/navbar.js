76204352929dda2155ddff9ea7f58b076a5d0725
Jacob Jonsson <jassob@live.se>
2015-07-15 22:21:28 +0200

    Changed some scripts.
    * Renamed script file to functions.js.
    * Included jQuery.js

--- static/dnollkse/js/navbar.js
+++
@@ -1,9 +0,0 @@
-function navbarClick() {
-  var navUl = document.getElementsByClassName("primary").item(0).children[1]
-
-  if (navUl.classList.contains("expanded")) {
-    navUl.classList.remove("expanded");
-  } else {
-    navUl.classList.add("expanded");
-  }
-}
