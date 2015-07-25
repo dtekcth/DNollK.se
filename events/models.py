72a7d7ad8809f96adebd85a47d3961c299d415bc
Jacob Jonsson <jassob@live.se>
2015-07-25 22:16:55 +0200

    Made the events be grouped by date in template.
    Achievement unlocked for exceeding the level of ugly code in this project.

--- events/models.py
+++ events/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import itertools
+
 class Event(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
@@ -8,3 +10,18 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_grouped_by_date():
+        """
+        Retrieves all events grouped by date.
+
+        Iterates all events and checks if the date of that event exists in grouped_events.
+        If that is the case it appends the event to that list, otherwise it creates a singleton
+        list with that event.
+        """
+        grouped_events = []
+        all_events = Event.objects.order_by('date')
+
+        for date, event_list in itertools.groupby(all_events, lambda e: e.date.date()):
+            grouped_events.append(list(event_list))
+        return grouped_events
--- events/views.py
+++ events/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from events.models import Event
 
 def index(request):
-    events = Event.objects.all()
+    events = Event.get_grouped_by_date()
     return render(request, "events/index.dtl", { 'events' : events })
