5656e389bf40292a09bda2159146ecf5f0bffcde
Jacob Jonsson <jassob@live.se>
2015-07-25 23:07:57 +0200

    Added a wysiwyg editor to events admin view.

--- events/admin.py
+++ events/admin.py
@@ -1,5 +1,24 @@
+# Django modules
 from django.contrib import admin
 
-from .models import Event
+# Our own modules
+from events.models import Event
 
-admin.site.register(Event)
+"""
+events.admin module
+
+This is the admin module for our events application.
+Here we can define admin classes for our models and customize how they appear in the admin interface.
+"""
+
+class EventAdmin(admin.ModelAdmin):
+    """
+    Event admin class.
+
+    We define that the model that we handle is Event and that we want to change the form to use django_wysiwyg.
+    """
+    model = Event
+    change_form_template = 'events/admin/change_form.html'
+
+# Register the models in our admin view
+admin.site.register(Event, EventAdmin)
