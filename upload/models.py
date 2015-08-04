e1f8a995746e4c2ab6f5f18c61509c4d3e18e989
tairoman <ygge95@gmail.com>
2015-08-04 20:45:37 +0200

    Added nametag to uploaded pictures

--- upload/models.py
+++ upload/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.core.files.storage import FileSystemStorage
+from dnollkse import settings
 
-f_storage = FileSystemStorage(location='/static/photos')
+f_storage = FileSystemStorage(location=settings.STATIC_ROOT + "/uploads/")
 
 class Upload(models.Model):
+    name = models.CharField(max_length=100)
     photo = models.ImageField(storage=f_storage)
 
