e22bb3397d6642b96e13edc46b91227617babc6b
tairoman <ygge95@gmail.com>
2016-07-18 11:37:32 +0200

    Added optional fields and donk and sektionen now has the same functionality

--- about/models.py
+++ about/models.py
@@ -1,21 +1,27 @@
 from django.db import models
 
 class Committee(models.Model):
+    """
+    The db model for a Committee. Choosing an image and description text is optional.
+    """
     name = models.CharField(max_length=100)
-    image = models.FileField(upload_to='%Y') # Upload to subfolder of %MEDIA_ROOT named after current year
-    text = models.TextField()
+    image = models.FileField(upload_to='%Y', blank=True) # Upload to subfolder of %MEDIA_ROOT named after current year
+    text = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
 
 
 class Member(models.Model):
+    """
+    The db model for a member of a committee. Optional field are description text, phone number and image.
+    """
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     nick = models.CharField(max_length=200)
-    text = models.TextField()
-    number = models.CharField(max_length=50)
-    image = models.FileField(upload_to='%Y')  # Upload to subfolder of %MEDIA_ROOT named after current year
+    text = models.TextField(blank=True)
+    number = models.CharField(max_length=50, blank=True)
+    image = models.FileField(upload_to='%Y', blank=True)  # Upload to subfolder of %MEDIA_ROOT named after current year
     position = models.CharField(max_length=50)
     committee = models.ForeignKey(Committee)
 
@@ -26,7 +32,7 @@ class Member(models.Model):
         return Member.objects.filter(committee=com)
 
     def get_full_name(self):
-            return self.first_name +  " " + self.last_name
+        return self.first_name +  " " + self.last_name
 
     def get_full_name_with_nick(self):
         return self.first_name + " \"" + self.nick + "\" " + self.last_name
