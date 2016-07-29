305fed55572e4fb9201542aa0da7f17be662f5da
Jacob Jonsson <jassob@live.se>
2016-07-29 22:33:45 +0200

    Made it possible to request past events.

--- events/views.py
+++ events/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 
 from events.models import Event
 
+"""
+Event view module.
+
+In this module we list the views that contains the logic to the templates
+and also provides them with the data they show.
+"""
+
 def index(request):
+    """
+    Gets all events for the current year and renders them on a page.
+    """
     events = Event.get_grouped_by_date()
     return render(request, "events/index.dtl", { 'events' : events })
+
+def index_from_year(request, year):
+    """
+    Gets all events for a requested year and renders them on a page.
+    """
+    events = Event.get_grouped_by_date_and_year(year)
+    return render(request, "events/index.dtl", { 'events' : events})
