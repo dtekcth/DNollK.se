58489590787ffe8d93ed7a71a6430b2362acbfb0
Jacob Jonsson <jassob@live.se>
2016-08-11 23:04:38 +0200

    More CSS and less javascript.
    Attempt to make more transitions use css instead of javascript.

--- static/dnollkse/js/functions.js
+++ static/dnollkse/js/functions.js
@@ -1,9 +1,5 @@
-function navbarClick() {
-  $('.primary ul').slideToggle("slow");
-}
-
 function toggleSidebar() {
-  if($('#content').is('.pushed')) {
+  if ($('#content').is('.pushed')) {
     $('#content').show();
   } else {
     setTimeout(function() {
@@ -16,3 +12,23 @@ function toggleSidebar() {
     $('#content').toggleClass("pushed");
   }, 0);
 }
+
+$( document ).ready(function() {
+
+  // We like haskell! :D
+  var h = $('.primary > ul').height();
+
+  $('.primary > ul').get(0).style.height = "0rem";
+
+  $('#nav-btn').click(function() {
+    if ($('.primary > ul').get(0).style.height == "0rem") {
+      setTimeout(function() {
+        $('.primary > ul').get(0).style.height = h + "px";
+      }, 0);
+    } else {
+      setTimeout(function() {
+        $('.primary > ul').get(0).style.height = "0rem";
+      }, 0);
+    }
+  });
+});
