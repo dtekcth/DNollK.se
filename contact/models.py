bf3d27003dd7de221cda3168b92b1f9768282ca5
Jacob Jonsson <jassob@live.se>
2017-07-11 21:55:09 +0200

    Added contact information to the db

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class ContactInfo(models.Model):
+    phone = models.CharField(10)
+    mail = models.EmailField()
+    address = models.TextField()
+    union_phone = models.CharField(10)
+    student_counselor_dtek = models.CharField(10)
+    curator = models.CharField(10)
+    chalmers_ward = models.CharField(10)
+    healthcare_info = models.CharField(10)
+    emergency_info = models.CharField(10)
