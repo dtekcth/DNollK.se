27e68fa8451b28bb07eaba2a3a1e4592ebcabdbb
Jacob Jonsson <jassob@live.se>
2017-07-18 23:15:26 +0200

    Made the config object less messy to work with.
    More options are optional.

--- dnollkse/models.py
+++ dnollkse/models.py
@@ -3,6 +3,12 @@
 from django.db import models
 import json
 
+def is_none_or_empty(str):
+    """
+    Checks if a string is None or empty string.
+    """
+    return not str
+
 
 class Config(models.Model):
     # Social widgets
@@ -10,22 +16,22 @@ class Config(models.Model):
     twitter_widget_id = models.TextField()
 
     # Calendar
-    gcal_api_key = models.TextField(null=True)
-    gcal_dnollk_calendar_id = models.TextField(null=True)
-    gcal_dnollk_calendar_url = models.TextField(null=True)
-    gcal_timeedit_calendar_id = models.TextField(null=True)
-    gcal_asp_calendar_id = models.TextField(null=True)
+    gcal_api_key = models.TextField(null=True, blank=True)
+    gcal_dnollk_calendar_id = models.TextField(null=True, blank=True)
+    gcal_dnollk_calendar_url = models.TextField(null=True, blank=True)
+    gcal_timeedit_calendar_id = models.TextField(null=True, blank=True)
+    gcal_asp_calendar_id = models.TextField(null=True, blank=True)
 
     # Form
-    gform_link = models.TextField(null=True)
-    gform_embed_link = models.TextField(null=True)
+    gform_link = models.TextField(null=True, blank=True)
+    gform_embed_link = models.TextField(null=True, blank=True)
 
     enable_form = models.BooleanField()
 
     # Logos and headers
     logo_url = models.TextField(default="/static/dnollkse/images/logo.png")
     header_url = models.TextField(default="/static/dnollkse/images/header.jpg")
-    favicon_url = models.TextField(null=True)
+    favicon_url = models.TextField(null=True, blank=True)
 
     year = models.CharField(max_length=4, default="2017")
 
@@ -33,44 +39,44 @@ class Config(models.Model):
         """
         Returns whether the dnollk calendar is enabled
         """
-        return self.gcal_api_key is not None \
-            and self.gcal_dnollk_calendar_id is not None \
-            and self.gcal_dnollk_calendar_url is not None
+        return not is_none_or_empty(self.gcal_api_key) \
+            and not is_none_or_empty(self.gcal_dnollk_calendar_id) \
+            and not is_none_or_empty(self.gcal_dnollk_calendar_url)
 
     def is_timeedit_calendar_enabled(self):
         """
         Returns whether the timeedit (school) calendar is enabled
         """
-        return self.gcal_api_key is not None \
-            and self.gcal_timeedit_calendar_id is not None
+        return not is_none_or_empty(self.gcal_api_key) \
+            and not is_none_or_empty(self.gcal_timeedit_calendar_id)
 
     def is_asp_calendar_enabled(self):
         """
         Returns whether the asp calendar is enabled
         """
-        return self.gcal_api_key is not None \
-            and self.gcal_asp_calendar_id is not None
+        return not is_none_or_empty(self.gcal_api_key) \
+            and not is_none_or_empty(self.gcal_asp_calendar_id)
 
     def calEventSources(self):
         """
         Returns a python object with the event sources used in FullCalender.
         """
         eventSources = []
-        if self.enable_dnollk_calendar:
+        if self.is_dnollk_calendar_enabled:
             eventSources.append({
                 'googleCalendarId': self.gcal_dnollk_calendar_id,
                 'className': 'eventCalendar',
                 'color': "#FA6607"
             })
 
-        if self.enable_timeedit_calendar:
+        if self.is_timeedit_calendar_enabled:
             eventSources.append({
                 'googleCalendarId': self.gcal_timeedit_calendar_id,
                 'className': 'schoolCalendar',
                 'color': 'CornFlowerBlue'
             })
 
-        if self.enable_asp_calendar:
+        if self.is_asp_calendar_enabled:
             eventSources.append({
                 'googleCalendarId': self.gcal_asp_calendar_id,
                 'className': 'aspCalendar'
