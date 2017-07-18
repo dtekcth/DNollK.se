bea5017251c5eb126539099bad788a16af6abf23
Jacob Jonsson <jassob@live.se>
2017-07-19 00:11:45 +0200

    Added views and admin views for documents

--- documents/admin.py
+++ documents/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
+from django.forms import ModelForm, TextInput, Textarea
 from documents.models import Document
+
+
+class DocumentForm(ModelForm):
+    """
+    Form for editing documents.
+    It mainly overrides the title to a textinput field.
+    """
+    class Meta:
+        model = Document
+        fields = ('title', 'body')
+        widgets = {
+            'title': TextInput
+        }
 
 
 class DocumentAdmin(admin.ModelAdmin):
     """
     Document admin class.
     """
-    model = Document
-    list_display = ('title', 'body')
-
+    form = DocumentForm
+    change_form_template = 'documents/admin/change_form.dtl'
 
 admin.site.register(Document, DocumentAdmin)
--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,11 @@
 from dnollkse.viewHelper import render
 
+from documents.models import Document
+
 # Create your views here.
+def index(request):
+    """
+    Retrieves all documents and renders them on the index view.
+    """
+    documents = Document.objects.all()
+    return render(request, "documents/index.dtl", {'items': documents})
