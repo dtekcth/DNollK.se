87b546a7876a3640b8068770e5c040c218d93f34
Jacob Jonsson <jassob@live.se>
2017-07-18 22:08:44 +0200

    Removed Document from dnollkse.
    This is a step in creating a separate documents app.

--- dnollkse/models.py
+++ dnollkse/models.py
@@ -4,22 +4,6 @@ from django.db import models
 import json
 
 
-class Document(models.Model):
-    title = models.TextField()
-    body = models.TextField()
-
-    def render(self):
-        html = ["<div class=\"card center-text\">",
-                "<h4><i>" + self.title + "</i></h4>",
-                "<p>" + self.body + "</p>",
-                "</div>"]
-
-        return "\n".join(html)
-
-    def __str__(self):
-        return self.title
-
-
 class Config(models.Model):
     # Social widgets
     instagram_widget_url = models.TextField()
