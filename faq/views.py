f14a10f2d0763e291c8be9f4cf4356d1fe22d5c9
Jacob Jonsson <jassob@live.se>
2017-08-11 18:20:04 +0200

    Updated faq/views.py to conform to PEP8.

--- faq/views.py
+++ faq/views.py
@@ -1,7 +1,7 @@
 from dnollkse.viewHelper import render
-
 from faq.models import Faq
+
 
 def faq(request):
     faq = Faq.objects.all()
-    return render(request, "faq/index.dtl", {'faq' : faq})
+    return render(request, "faq/index.dtl", {'faq': faq})
